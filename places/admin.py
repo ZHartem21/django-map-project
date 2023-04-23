fced96a6c867639b7a84d10ed0a39d56a717a5c5
ZHartem21 <zhartem21@gmail.com>
2023-04-23 20:12:09 +0300

    Add drag and drop sorting with adminsortable2
    Required to add files for django 4.2 in env\Lib\site-packages\adminsortable2\templates\adminsortable2\edit_inline by copying template tabular-django-4.1 and renaming it to tabular-django-4.2

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,12 @@
+from adminsortable2.admin import SortableAdminMixin, SortableTabularInline, SortableAdminBase
 from django.contrib import admin
-from .models import Place, Image
 from django.utils import safestring
+
+from .models import Image, Place
 
 # Register your models here.
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableTabularInline, admin.TabularInline):
     model = Image
 
     readonly_fields = ['preview_image']
@@ -12,22 +14,22 @@ class ImageInline(admin.TabularInline):
     def preview_image(self, obj):
         return safestring.mark_safe('<img src="{url}" width="200" height="200" />'.format(
             url = obj.image_file.url,
-            )
+        )
     )
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
     inlines = [
         ImageInline,
     ]
 
 
 @admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
     readonly_fields = ['preview_image']
 
     def preview_image(self, obj):
         return safestring.mark_safe('<img src="{url}" width="200" height="200" />'.format(
             url = obj.image_file.url,
-            )
+        )
     )
--- places/models.py
+++ places/models.py
@@ -12,13 +12,19 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = 'places'
 
 
 class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
     image_file = models.ImageField()
-    number = models.IntegerField()
+    number = models.IntegerField(default=0, blank=False, null=False)
 
     def __str__(self):
-        return f'{self.number} {self.place}'
+        return f'{self.place}'
 
+    class Meta:
+        ordering = ['number']
