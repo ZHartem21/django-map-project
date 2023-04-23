1525906b3e737b1974cd3ab727f997eb5b08597d
ZHartem21 <zhartem21@gmail.com>
2023-04-23 15:29:31 +0300

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -2,5 +2,5 @@
 from django.shortcuts import render
 
 
-def start_page(request):
-    return render(request, 'start_page.html')
+def index(request):
+    return render(request, 'index.html')
