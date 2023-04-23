6940474fc22c40c7e4a1d9f535fc7394d67d1c80
ZHartem21 <zhartem21@gmail.com>
2023-04-23 17:04:33 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -12,3 +12,13 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image_file = models.ImageField()
+    number = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.number} {self.place}'
+
