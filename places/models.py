790e7dbccc478420aec84c3b21a11afa84f21ef4
ZHartem21 <zhartem21@gmail.com>
2023-04-28 12:21:27 +0300

    Edit models
    Allow description to be blank
    Reorder atributes
    Delete unnecessary atributes for integer field
    Disallow image_file to be blank

--- places/models.py
+++ places/models.py
@@ -4,26 +4,26 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
-    description_short = models.TextField()
-    description_long = HTMLField()
+    description_short = models.TextField(blank=True)
+    description_long = HTMLField(blank=True)
     lat = models.FloatField(max_length=100)
     lon = models.FloatField(max_length=100)
-
-    def __str__(self):
-        return self.title   
     
     class Meta:
         ordering = ['pk']
         unique_together = [['lat', 'lon']]
 
+    def __str__(self):
+        return self.title   
+
 
 class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, null=True, blank=True)
-    image_file = models.ImageField(null=True, blank=True)
-    number = models.IntegerField(default=0, blank=False, null=False)
-
-    def __str__(self):
-        return f'{self.place}'
+    image_file = models.ImageField(null=True)
+    number = models.IntegerField(default=0)
 
     class Meta:
         ordering = ['number']
+
+    def __str__(self):
+        return f'{self.place}'
