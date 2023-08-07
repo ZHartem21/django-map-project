8bbcd766d4739361a93ecf716341cbc1b298ca76
ZHartem21 <zhartem21@gmail.com>
2023-08-08 01:55:26 +0300

    Use KeyError and TypeError exception for load

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -19,26 +19,32 @@ class Command(BaseCommand):
         response = requests.get(place_file)
         response.raise_for_status()
         place_details = response.json()
-        if not place_details['title']:
+        try:
+            if not place_details['title']:
+                raise KeyError
+            place, created = Place.objects.get_or_create(
+                title=place_details['title'],
+                defaults={
+                    'description_short': place_details.get('description_short'),
+                    'description_long': place_details.get('description_long'),
+                    'lat': place_details['coordinates']['lat'],
+                    'lon': place_details['coordinates']['lng'],
+                },
+            )
+            return place_details.get('imgs'), place, created
+        except (KeyError, TypeError):
             return
-        place, created = Place.objects.get_or_create(
-            title=place_details['title'],
-            defaults={
-                'description_short': place_details.get('description_short'),
-                'description_long': place_details.get('description_long'),
-                'lat': place_details['coordinates']['lat'],
-                'lon': place_details['coordinates']['lng'],
-            },
-        )
-        return place_details.get('imgs'), place, created
 
     def add_arguments(self, parser):
         parser.add_argument('file_address', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        for place_file in options['file_address']:
-            place_image_urls, place, created = self.load_place_meta(place_file=place_file)
-            for image_url in place_image_urls:
-                self.load_place_images(image_url=image_url, place=place)
+        try:
+            for place_file in options['file_address']:
+                place_image_urls, place, created = self.load_place_meta(place_file=place_file)
+                for image_url in place_image_urls:
+                    self.load_place_images(image_url=image_url, place=place)
 
-            self.stdout.write(self.style.SUCCESS(f'New: {created}. Created or updated place {place} '))
+                self.stdout.write(self.style.SUCCESS(f'New: {created}. Created or updated place {place} '))
+        except (KeyError, TypeError):
+            return
