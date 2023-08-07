c1ddb12df9f333b05d4f8308139d236416c07420
ZHartem21 <zhartem21@gmail.com>
2023-08-08 02:24:58 +0300

    Move cycle to method

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,38 +1,42 @@
 from django.core.management.base import BaseCommand
 from django.core.files.base import ContentFile
 from places.models import Place, Image
+from django.db.utils import IntegrityError
 import requests
 
 
 class Command(BaseCommand):
     help = 'Helps to quickly create new place of interest'
 
-    def load_place_images(self, image_url, place):
-        response = requests.get(image_url)
-        response.raise_for_status()
-        image_name = response.url.split('/')[-1]
-        image_content = ContentFile(response.content, name=image_name)
-        image = Image.objects.create(place=place, image_file=image_content)
-        self.stdout.write(self.style.SUCCESS(f'Created image for {image} '))
+    def load_place_images(self, place_image_urls, place):
+        try:
+            for image_url in place_image_urls:
+                response = requests.get(image_url)
+                response.raise_for_status()
+                image_name = response.url.split('/')[-1]
+                image_content = ContentFile(response.content, name=image_name)
+                image = Image.objects.create(place=place, image_file=image_content)
+                self.stdout.write(self.style.SUCCESS(f'Created image for {image} '))
+        except requests.exceptions.HTTPError:
+            return
 
     def load_place_meta(self, place_file):
         response = requests.get(place_file)
         response.raise_for_status()
         place_details = response.json()
         try:
-            if not place_details['title']:
-                raise KeyError
             place, created = Place.objects.get_or_create(
                 title=place_details['title'],
                 defaults={
-                    'description_short': place_details.get('description_short'),
-                    'description_long': place_details.get('description_long'),
+                    'description_short': place_details.get('description_short', 'No short description'),
+                    'description_long': place_details.get('description_long', 'No long description'),
                     'lat': place_details['coordinates']['lat'],
                     'lon': place_details['coordinates']['lng'],
                 },
             )
-            return place_details.get('imgs'), place, created
-        except (KeyError, TypeError):
+            return place_details.get('imgs', 'no-image'), place, created
+        except (KeyError, TypeError, IntegrityError):
+            self.stdout.write(self.style.ERROR('Failed to load place'))
             return
 
     def add_arguments(self, parser):
@@ -42,9 +46,8 @@ class Command(BaseCommand):
         try:
             for place_file in options['file_address']:
                 place_image_urls, place, created = self.load_place_meta(place_file=place_file)
-                for image_url in place_image_urls:
-                    self.load_place_images(image_url=image_url, place=place)
+                self.load_place_images(place_image_urls, place=place)
 
                 self.stdout.write(self.style.SUCCESS(f'New: {created}. Created or updated place {place} '))
-        except (KeyError, TypeError):
-            return
+        except (KeyError, TypeError, IntegrityError):
+            self.stdout.write(self.style.ERROR('Failed to create or update place'))
